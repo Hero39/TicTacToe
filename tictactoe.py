8b84b78acbb12e9682c9f3f75a05b0dde614e4bd
Hero Brouwer <hero.brouwer.student@uva.nl>
2024-07-06 12:03:44 +0200

    updates

--- tictactoe.py
+++ tictactoe.py
@@ -3,15 +3,30 @@
 
 
 grid = [
-    [' ', ' ', ' '],
+    ['_', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
 ]
 
 
 def create_grid():
+
+    indices_rows = [i for i, x in enumerate(grid)]
+    print(indices_rows)
     for row in grid:
         print('|'.join(row))
         print('-' * 5)
 
+# def move_in_grid(x, y):
+    
+    
+# def move_with_key():
+
 create_grid()
+
+
+def game():
+    playing = True
+    
+    
+    
