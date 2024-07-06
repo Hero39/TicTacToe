857752b7f08585bae295266dd72fa3744600aaee
Hero Brouwer <hero.brouwer.student@uva.nl>
2024-07-06 11:28:25 +0200

    begin

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+# Name: Hero Brouwer
+# Description: This program implements Tic-Tac-Toe.
+
+def show_grid():
+    grid = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+        [' ', ' ', ' ']
+    ]
+    return grid
+
+print(show_grid)
